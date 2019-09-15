96b8a5fb04002fc21d7576e369a3b637bb7d7728
cainscreation <48478145+cainscreation@users.noreply.github.com>
2019-09-15 18:53:41 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SignIn|SignUp</title>
+    <link rel="shortcut icon" href="images\logo.png">
+  </head>
+  <body>
+<div class="login">
+  <h2>Login here</h2>
+  <input type="username" name="User" value="" required>
+  <input type="password" name="Pass" value="" required>
+<button type="submit" name="button">Login</button>
+</div>
+
+<div class="register">
+  <h2>Register here</h2>
+  <input type="username" name="User" value="" required>
+  <input type="password" name="Pass" value="" required>
+  <button type="submit" name="button">SignUp</button>
+
+</div>
+
+  </body>
+</html>
